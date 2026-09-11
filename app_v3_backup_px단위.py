b3df026d3cc55cef33e5d7403fc0d40b89e0d76e
hyowon-1592 <hyowon@quazar.co.kr>
2026-09-11 09:03:43 +0900

    Update app_v3_backup_px단위.py

--- app_v3_backup_px단위.py
+++ app_v3_backup_px단위.py
@@ -136,7 +136,8 @@ def load_data_location(file_path):
     if not text_data: return None, None
     
     data_list = []
-    pattern = re.compile(r"\[(.*?)\]\s*중심 x:\s*[\d\.]+,\s*빨간점 x:\s*[\d\.]+,\s*x 차이:\s*([\d\.]+)")
+    # pattern = re.compile(r"\[(.*?)\]\s*중심 x:\s*[\d\.]+,\s*빨간점 x:\s*[\d\.]+,\s*x 차이:\s*([\d\.]+)")
+    pattern = re.compile(r"\[(.*?)\].*?측정 차이:\s*([\d\.]+)px\[(.*?)\].*?")
     for line in text_data.split('\n'):
         match = pattern.search(line.strip())
         if match:
